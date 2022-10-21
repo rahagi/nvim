fd84c8a96faac79c05ce95111e93a7bdff3df9e4
Rahagi <rahagi@protonmail.com>
2022-10-21 11:29:34 +0700

    feat: treesitter context

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,24 @@
 local configs = require("nvim-treesitter.configs")
+local context = require("treesitter-context")
 
 configs.setup({})
+context.setup({
+	max_lines = 2,
+	patterns = {
+		default = {
+			"function",
+			"method",
+			"for",
+			"while",
+			"if",
+			"else",
+			"try",
+			"except",
+			"finally",
+			"with",
+			"match",
+		},
+	},
+})
+
+vim.cmd("hi def TreesitterContext ctermbg=233")
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -179,6 +179,16 @@ _G.packer_plugins = {
     path = "/home/rhg/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-treesitter-context"] = {
+    loaded = true,
+    path = "/home/rhg/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
+  },
+  ["nvim-treesitter-textobjects"] = {
+    loaded = true,
+    path = "/home/rhg/.local/share/nvim/site/pack/packer/start/nvim-treesitter-textobjects",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/rhg/.local/share/nvim/site/pack/packer/start/packer.nvim",
